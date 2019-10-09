e3becdfa046940681b65fa9f694ad679cc19d69e
Oliver <molivera@8amarketing.com>
2019-10-09 16:01:05 -0300

    agrego propiedades de usuario a negocio

--- Negocio/Usuarios.cs
+++ Negocio/Usuarios.cs
@@ -8,6 +8,17 @@ namespace Negocio
 {
     public class Usuarios
     {
+        #region Propiedades
+        public int? Id { get; set; }
+        public string Nombre{ get; set; }
+        public string Apellido { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string DNI { get; set; }
+        public string Email{ get; set; }
+        public string Password{ get; set; }
+        #endregion
+
         public bool Validar(string email, string contrasena)
         {
             // valida contra la capa de datos y devuelve true o false si existe
