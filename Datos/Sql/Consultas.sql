5ef5b2cc30dc53f27eecdf84cffb6d640f6acb5e
Oliver <molivera@8amarketing.com>
2019-11-20 10:11:52 -0300

    agrego consultas

---
+++ Datos/Sql/Consultas.sql
@@ -0,0 +1,19 @@
+﻿-- materias de alumno
+select m.*
+from Materias m
+inner join Usuario_Materia um on um.materia_id = m.Materia_id
+inner join users u on u.user_id = um.user_id
+where u.user_id = ?
+
+-- alumnos de una materia
+select u.*, em.Descripcion as estado_materia
+from Materias m
+inner join Usuario_Materia um on um.materia_id = m.Materia_id
+inner join users u on u.user_id = um.user_id
+inner join Estado_Materia em on em.Estado_Materia_Id = um.Estado_Materia_id
+where m.Materia_id = ?
+
+-- obtener usuarios por rol
+select u.*
+from users u
+where u.rol_id = ?
